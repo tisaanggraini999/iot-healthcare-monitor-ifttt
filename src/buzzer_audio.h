45ea3301559e91f66e3ecd813a421599cd8aac0d
Tisa Anggraini <tisaanggraini2001@gmail.com>
2025-12-15 09:15:51 +0700

    Add buzzer audio functions and initialization

--- src/buzzer_audio.h
+++ src/buzzer_audio.h
@@ -1,1 +1,19 @@
+#ifndef BUZZER_AUDIO_H
+#define BUZZER_AUDIO_H
+
+int BUZZER_PIN = 18;
+
+void initBuzzer() {
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void beepOK() {
+  tone(BUZZER_PIN, 2000, 150);
+}
+
+void beepError() {
+  tone(BUZZER_PIN, 1000, 500);
+}
+
+#endif
 
