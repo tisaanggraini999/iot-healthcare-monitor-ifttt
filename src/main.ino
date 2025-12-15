f9bc5596d576667de3588af8c30d2ccc0f0a4da3
Tisa Anggraini <tisaanggraini2001@gmail.com>
2025-12-15 09:16:10 +0700

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,28 +1,41 @@
-#include <Arduino.h>
 #include "wifi_config.h"
 #include "ifttt_client.h"
 #include "sensor_bp.h"
+#include "actuator_control.h"
+#include "lcd_display.h"
+#include "buzzer_audio.h"
 
 int systolic;
 int diastolic;
 
 void setup() {
   Serial.begin(115200);
+
   connectWiFi();
+  initActuator();
+  initLCD();
+  initBuzzer();
 }
 
 void loop() {
+  openValve();
+  delay(500);
+  startPump();
+  delay(3000);
+
   systolic = readPressure();
+  stopPump();
+  closeValve();
+
   diastolic = systolic * 2 / 3;
 
-  Serial.print("SYS: ");
-  Serial.print(systolic);
-  Serial.print(" DIA: ");
-  Serial.println(diastolic);
-
   if (systolic > 50 && systolic < 250) {
-    sendToIFTTT(systolic, diastolic, 0);
+    showPressure(systolic, diastolic);
+    sendToIFTTT(systolic, diastolic);
+    beepOK();
+  } else {
+    beepError();
   }
 
-  delay(15000); // avoid spam
+  delay(15000);
 }
