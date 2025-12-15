dc20a9c2b7abde0ff1b57834639e6acbf6563c28
Tisa Anggraini <tisaanggraini2001@gmail.com>
2025-12-15 09:14:18 +0700

    Modify readPressure function for offset and validation
    Added offset pressure adjustment and negative value check.

--- src/sensor_bp.h
+++ src/sensor_bp.h
@@ -2,11 +2,13 @@
 #define SENSOR_BP_H
 
 int sensorPin = 26;
+int offsetPressure = 20;
 
 int readPressure() {
   int raw = analogRead(sensorPin);
   float voltage = raw * 50.0 / 4095.0;
-  int pressure = voltage / 12;
+  int pressure = (voltage - offsetPressure) / 12;
+  if (pressure < 0) pressure = 0;
   return pressure;
 }
 
