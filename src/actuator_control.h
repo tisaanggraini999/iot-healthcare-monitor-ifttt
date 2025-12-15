7ad59c03eaae2668f3865336e872c88586723d05
Tisa Anggraini <tisaanggraini2001@gmail.com>
2025-12-15 09:14:43 +0700

    Add actuator control functions and definitions

--- src/actuator_control.h
+++ src/actuator_control.h
@@ -1,1 +1,31 @@
-d
+#ifndef ACTUATOR_CONTROL_H
+#define ACTUATOR_CONTROL_H
+
+int PUMP_PIN  = 4;
+int VALVE_PIN = 32;
+
+void initActuator() {
+  pinMode(PUMP_PIN, OUTPUT);
+  pinMode(VALVE_PIN, OUTPUT);
+  digitalWrite(PUMP_PIN, LOW);
+  digitalWrite(VALVE_PIN, LOW);
+}
+
+void startPump() {
+  digitalWrite(PUMP_PIN, HIGH);
+}
+
+void stopPump() {
+  digitalWrite(PUMP_PIN, LOW);
+}
+
+void openValve() {
+  digitalWrite(VALVE_PIN, HIGH);
+}
+
+void closeValve() {
+  digitalWrite(VALVE_PIN, LOW);
+}
+
+#endif
+
